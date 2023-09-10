a06493a9bdee4504c76a68f622ddd2bcde648726
devroy73 <devroy73@users.noreply.github.com>
2023-09-10 19:12:37 +0100

    Update chatbotapp.py

--- chatbotapp.py
+++ chatbotapp.py
@@ -52,7 +52,7 @@ with st.sidebar:
 
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content":'vas a aprender la diferencia entre ser y estar.' }]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -65,7 +65,7 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
