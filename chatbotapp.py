402a53b706d6058571a01955428598ff62f8ecd7
devroy73 <devroy73@users.noreply.github.com>
2023-09-10 19:31:15 +0100

    Update chatbotapp.py

--- chatbotapp.py
+++ chatbotapp.py
@@ -48,7 +48,7 @@ with st.sidebar:
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-  
+
 openai.api_key=st.secrets["api"]
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
@@ -58,9 +58,10 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    # if not openai_api_key:
-    #     st.info("Please add your OpenAI API key to continue.")
-    #     st.stop()
+    if not openai_api_key:
+        st.info(" the api key is :",openai.api_key)
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
 
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
